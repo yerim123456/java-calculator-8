d3516038d9967cdf5e9f8a59c6df7b66297ae519
yerim123456 <yearim1225@gmail.com>
2025-10-20 22:44:23 +0900

    feat: 사용자와 연결을 위한 문자열 입력 및 결과값 출력 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 안내 메시지 출력
+        System.out.println("덧셈할 숫자를 구분자와 함께 입력해주세요\n"
+                + "기본 구분자는 : 와 , 이며 커스텀 구분자를 설정하고 싶으시다면 "
+                + "문장 맨 앞에 // 문자와 \\n 문자 사이에 1개의 문자를 설정해주시면 됩니다.");
+
+        // 2. 문자열 입력 받기
+        String input = Console.readLine();
+        if (input == null) {
+            throw new IllegalArgumentException("null 값은 허용되지 않습니다.");
+        }
+
+        // 9. 결과값 반환
+        int result = Calculator.add(input);
+        System.out.println("결과 : " + result);
     }
 }
